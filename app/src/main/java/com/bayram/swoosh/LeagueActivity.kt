7f729897e667e50d5c3eefd574cb2663aefd101c
Bayram Jumayew <bjumayew@gmail.com>
2023-03-10 11:07:36 +0500

    Skills Activity

--- app/src/main/java/com/bayram/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/bayram/swoosh/LeagueActivity.kt
@@ -1,13 +1,26 @@
 package com.bayram.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.bayram.swoosh.databinding.ActivityLeagueBinding
 
 
 class LeagueActivity : BaseActivity() {
+    lateinit var binding: ActivityLeagueBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLeagueBinding.inflate(LayoutInflater.from(this))
         setContentView(R.layout.activity_league)
+
+        fun leagueNextClicked(view: View) {
+             val skillActivity = Intent(this, SkillsActivity::class.java)
+            startActivity(skillActivity)
+            
+
+        }
 
     }
 
